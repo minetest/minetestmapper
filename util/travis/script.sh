3e8720313b89e37dd1c15e812d684eae9715a55a
sfan5 <sfan5@live.de>
2019-07-30 20:49:32 +0200

    Move travis to newest Ubuntu bionic

--- util/travis/script.sh
+++ util/travis/script.sh
@@ -1,15 +1,8 @@
 #!/bin/bash -e
-
-CXX=g++-7
-[ $CC == "clang" ] && CXX=clang++-5.0
-export CXX
-
-
 mkdir -p travisbuild
 cd travisbuild
 
-cmake \
-	-DENABLE_LEVELDB=1 \
-	..
+cmake .. \
+	-DENABLE_LEVELDB=1
 
 make -j2
